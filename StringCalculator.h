6269944508f7c9d93cf5bf49e73703a05ad8bb76
RakshithR98 <rakshithraghu2017@gmail.com>
2024-10-18 11:27:00 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,16 @@
-
+#define STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+ 
+#include <string>
+#include <cctype>
+#include <iostream>
+ 
+class StringCalculator
+{
+  public:
+    int less_than_thousand(int num);
+    int get_positive_number(int num);
+    std::string processed_String_for_delimiters(std::string input);
+    int exception_handling_for_whitespce(std::string token);
+    int add(std::string input);
+};
